8daf6191c613bd9e16db8e55a115ae4908e53cfa
leaving-keys <hypho.13@gmail.com>
2024-02-05 09:48:42 +0000

    Update DAG to use Airflow variables.

--- chapter_3/my_csv_dag.py
+++ chapter_3/my_csv_dag.py
@@ -1,25 +1,32 @@
 """
-A simple Airflow DAG that reads a *.csv file then converts it to a *.json file.
+A simple Airflow DAG that converts a *.csv file into JSON.
 """
-
 import datetime
 from datetime import timedelta
+import os
 
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable
 
 import pandas
 
 
 def csv_to_json():
+    # Import the PATH_TO_ROOT Airflow.
+    path_to_root = Variable.get("PATH_TO_ROOT")
+
+    # Read the *.csv file using pandas.
     df = pandas.read_csv(
-        "~/data_engineering_with_python/chapter_3/csv_files/data.csv"
+        os.path.join(path_to_root, "chapter_3", "csv_files", "data.csv")
     )
     for index, row in df.iterrows():
         print(row["name"])
+    
+    # Convert data into JSON.
     df.to_json(
-        "~/data_engineering_with_python/chapter_3/json_files/from_airflow.json",
+        os.path.join(path_to_root, "chapter_3", "json_files", "from_airflow.json"), 
         orient="records"
     )
 
@@ -30,7 +37,6 @@ default_args = {
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
-
 with DAG(
         "MyCsvDag",
         default_args=default_args,
